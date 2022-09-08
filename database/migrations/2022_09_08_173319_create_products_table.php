a0f8a581e31aad8ab07c1da41062f55ca9432e8d
Jaas <mrngc2068@gmail.com>
2022-09-08 13:27:30 -0500

    Products:: Created migration, model, controller and resource route.

---
+++ database/migrations/2022_09_08_173319_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku');
+            $table->string('name');
+            $table->unsignedInteger('category_id');
+            $table->text('description');
+            $table->decimal('price')->default(0);
+            $table->integer('quantity')->default(0);
+            $table->boolean('stock')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
